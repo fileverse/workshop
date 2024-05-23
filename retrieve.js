b94b9090b07f37762f91bbb8d491d7ac2ae29d63
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2024-05-23 17:19:34 +0530

    added scripts to retrieve and replicate step

---
+++ retrieve.js
@@ -0,0 +1,26 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const { getContract } = require('viem');
+const PortalABI = require('./abi');
+const { publicClient } = require('./client');
+
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const FILE_ID = process.env.FILE_ID;
+
+const contract = getContract({
+  address: CONTRACT_ADDRESS,
+  abi: PortalABI,
+  client: publicClient,
+});
+
+(async () => {
+  const fileData = await contract.read.files([FILE_ID]);
+  console.log({
+    metadataIPFSHash: fileData[0],
+    contentIPFSHash: fileData[1],
+    gateIPFSHash: fileData[2],
+    fileType: fileData[3] === 0 ? 'public' : 'private',
+  });
+})();
